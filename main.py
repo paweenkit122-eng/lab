d2be26c6df56aaaac7bd14e55e02b58bd6f55120
Newraiwa <paweenkit.t@ku.th>
2026-04-23 14:04:22 +0700

    show news list

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from datetime import datetime
 
-app = Flask(__name__)
+news_items = {
+    1: {'id': 1, 'title': 'News 1', 'body': 'Detail 1'},
+    2: {'id': 2, 'title': 'News 2', 'body': 'Detail 2'},
+}
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        name="Somchai",
+        time=datetime.now(),
+        news_items=news_items.values()
+    )
